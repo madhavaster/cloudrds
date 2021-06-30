39e41b99576284407b5f93c4968e15d0e381dcf1
Madhava Reddy <madhava@agrisoft.com>
2021-06-30 20:12:51 +0530

    Initial commit

---
+++ src/main/java/com/agrisutra/CloudrdsApplication.java
@@ -0,0 +1,13 @@
+package com.agrisutra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudrdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudrdsApplication.class, args);
+	}
+
+}
